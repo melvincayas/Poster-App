5066eb48300b3dda510fac763ff9ec1a6441ed53
Melvin Cayas <cayasmj@gmail.com>
2021-05-15 22:33:38 -0400

    Styling and Comment Bubbles

--- controllers/authenticate.js
+++ controllers/authenticate.js
@@ -1,37 +1,38 @@
-const handleAsync = require('../utilities/handleAsync');
-const ExpressError = require('../utilities/ExpressError');
-const User = require('../models/User');
+const handleAsync = require("../utilities/handleAsync");
+const ExpressError = require("../utilities/ExpressError");
+const User = require("../models/User");
 
 module.exports.registerForm = (req, res) => {
-    res.render('register');
-}
+	res.render("register");
+};
 
 module.exports.createUser = handleAsync(async (req, res, next) => {
-    const { username, password, email } = req.body;
-    const user = new User({ username, password, email });
-    await user.save();
-    req.session.user_id = user._id;
-    req.flash('success', 'Welcome to Poster!');
-    res.redirect('/posts');
-})
+	const { username, password, email } = req.body;
+	const user = new User({ username, password, email });
+	await user.save();
+	req.session.user_id = user._id;
+	req.flash("success", `Welcome to Poster, ${user.username}!`);
+	res.redirect("/posts");
+});
 
 module.exports.loginForm = handleAsync(async (req, res, next) => {
-    res.render('login');
-})
+	res.render("login");
+});
 
 module.exports.login = handleAsync(async (req, res, next) => {
-    const { username, password } = req.body;
-    const validUser = await User.findAndValidate(username, password);
-    if (validUser) {
-        req.session.user_id = validUser._id;
-        res.redirect('/posts');
-    } else {
-        req.flash('error','Invalid username or password.');
-        res.redirect('/login');
-    }
-})
+	const { username, password } = req.body;
+	const validUser = await User.findAndValidate(username, password);
+	if (validUser) {
+		req.session.user_id = validUser._id;
+		req.flash("success", `Welcome back, ${username}!`);
+		res.redirect("/posts");
+	} else {
+		req.flash("error", "Invalid username or password.");
+		res.redirect("/login");
+	}
+});
 
 module.exports.logout = (req, res) => {
-    req.session.user_id = null;
-    res.redirect('/login');
-}
+	req.session.user_id = null;
+	res.redirect("/login");
+};
