ec4fd9515486722e8c5e413efe009cd373cf5c9d
Melvin Cayas <cayasmj@gmail.com>
2021-05-25 19:17:56 -0400

    Added start script and port

--- index.js
+++ index.js
@@ -93,6 +93,7 @@ app.use((err, req, res, next) => {
 	res.render("error", { message, status });
 });
 
-app.listen(3000, () => {
-	console.log("Listening on Port 3000!");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log(`Listening on Port ${port}!`);
 });
